b3372f3ab923e7a50977c4ba3929ba3cd229cd24
lalithsivesh123-lab <lalithsivesh123@gmail.com>
2026-03-31 23:19:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,38 @@
+from binance.client import Client
+import pandas as pd
 import time
+import os
+
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
 
 print("🚀 BOT STARTED")
 
+client = Client(API_KEY, API_SECRET)
+
+SYMBOL = "BTCUSDT"
+
+def get_data():
+    klines = client.get_klines(symbol=SYMBOL, interval="1m", limit=50)
+    df = pd.DataFrame(klines)
+    df[4] = df[4].astype(float)
+    return df
+
 while True:
-    print("🔥 BOT IS RUNNING...")
-    time.sleep(5)
+    try:
+        df = get_data()
+        price = df[4].iloc[-1]
+        avg = df[4].mean()
+
+        print(f"Price: {price} | Avg: {avg}")
+
+        if price > avg:
+            print("📈 Signal: BUY")
+        else:
+            print("📉 Signal: SELL")
+
+        time.sleep(10)
+
+    except Exception as e:
+        print("❌ ERROR:", e)
+        time.sleep(5)
